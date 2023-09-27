63868fb081f19e09a552748fbfa1e1cf92778360
efmomoh <efmomoh25@gmail.com>
2023-09-27 11:00:29 +0000

    TodosLogic.jsx

---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,16 @@
+// Direct child component of TodoApp
+
+import InputTodo from '@/components/InputTodo';
+import TodosList from '@/components/TodosList';
+
+import { TodosProvider } from '@/context/TodosContext';
+
+const TodosLogic = () => {
+  return (
+    <TodosProvider>
+      <InputTodo />
+      <TodosList />
+    </TodosProvider>
+  );
+};
+export default TodosLogic;
